02da0dd90e1de16a47f0d85930eee95246fce10f
Blaine Glasgow <glasgowb@outlook.com>
2023-02-07 17:44:22 -0800

    Add travel array

---
+++ friends.rb
@@ -0,0 +1,11 @@
+arrays = ["David", "Rachel", "Doug", "Laura", "Andrew"]
+
+puts arrays[2]
+puts arrays[4]
+puts arrays[0]
+
+travel = ["Vietnam", "Mallorca", "Squammish", "Red River Gorge", "Mt. Erie"]
+
+travel.shift
+
+puts travel
